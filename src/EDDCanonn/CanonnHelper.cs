1b5c037a5a030cea9088259e1a57064e021c2a58
Flynn <sindri.weinstein@gmail.com>
2025-01-28 01:54:43 +0100

    implement static GetJObjectList

--- src/EDDCanonn/CanonnHelper.cs
+++ src/EDDCanonn/CanonnHelper.cs
@@ -1,5 +1,7 @@
 ﻿
 using System.Collections.Generic;
+using System.Linq;
+using QuickJSON;
 namespace EDDCanonn
 {
     public static class CanonnHelper
@@ -7,5 +9,13 @@ namespace EDDCanonn
         public static readonly string WhitelistUrl = "https://us-central1-canonn-api-236217.cloudfunctions.net/postEventWhitelist";
         public static readonly string EventPushUrl = "https://us-central1-canonn-api-236217.cloudfunctions.net/postEvent";
         public static readonly string PatrolUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQsi1Vbfx4Sk2msNYiqo0PVnW3VHSrvvtIRkjT-JvH_oG9fP67TARWX2jIjehFHKLwh4VXdSh0atk3J/pub?gid=0&single=true&output=tsv";
+   
+        public static List<JObject> GetJObjectList(JObject source, string key)
+        {
+            if (source[key] is JArray array)
+                return array.OfType<JObject>().ToList();
+            return null;
+        }
+
     }
 }
