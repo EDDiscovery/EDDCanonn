cbd5556db0db2cf11f82430465dc65d90ce34e12
Flynn <sindri.weinstein@gmail.com>
2025-02-04 21:59:02 +0100

    switch to double

--- src/EDDCanonn/Base/Patrols.cs
+++ src/EDDCanonn/Base/Patrols.cs
@@ -1,23 +1,36 @@
-﻿using System;
+﻿/*
+ * Copyright © 2022-2022 EDDiscovery development team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using KdTree;
 using KdTree.Math;
 
-
 namespace EDDCanonn.Base
 {
     public class Patrols
     {
-        public Dictionary<string, KdTree<float, Patrol>> CategoryTrees { get; set; } = new Dictionary<string, KdTree<float, Patrol>>();
+        public Dictionary<string, KdTree<double, Patrol>> CategoryTrees { get; set; } = new Dictionary<string, KdTree<double, Patrol>>();
 
         public Patrols()
         {
-            CategoryTrees["all"] = new KdTree<float, Patrol>(3, new FloatMath());
+            CategoryTrees["all"] = new KdTree<double, Patrol>(3, new DoubleMath());
         }
 
         private readonly object _addLock = new object();
-        public void Add(string category, KdTree<float, Patrol> tree)
+        public void Add(string category, KdTree<double, Patrol> tree)
         {
             lock (_addLock)
             {
@@ -25,7 +38,7 @@ namespace EDDCanonn.Base
                     return;
                 CategoryTrees[category] = tree;
 
-                foreach (KdTreeNode<float, Patrol> node in tree)
+                foreach (KdTreeNode<double, Patrol> node in tree)
                     CategoryTrees["all"].Add(node.Point, node.Value);
             }
         }
@@ -36,52 +49,64 @@ namespace EDDCanonn.Base
 
                 SearchCategory(category, x, y, z, maxDistance, result);
 
-            return result.OrderBy(t => t.distance).Take(500).ToList();
+            return result.ToList();
         }
 
         private void SearchCategory(string category, double x, double y, double z, double maxDistance, List<(string, Patrol, double)> result)
         {
             if (!CategoryTrees.ContainsKey(category)) return;
 
-            float[] searchPoint = new[] { (float)x, (float)y, (float)z };
-            KdTreeNode<float, Patrol>[] nodesInRange = CategoryTrees[category].RadialSearch(searchPoint, (float)maxDistance);
+            double[] searchPoint = new[] { x, y, z };
+            KdTreeNode<double, Patrol>[] nodesInRange = CategoryTrees[category].RadialSearch(searchPoint, maxDistance);
 
-            foreach (KdTreeNode<float, Patrol> node in nodesInRange)
+            foreach (KdTreeNode<double, Patrol> node in nodesInRange)
             {
                 double distance = CalculateDistance(searchPoint, node.Point);
                 result.Add((category, node.Value, distance));
             }
         }
 
-        private double CalculateDistance(float[] point1, float[] point2)
+        private double CalculateDistance(double[] point1, double[] point2)
         {
             double sum = 0;
             for (int i = 0; i < point1.Length; i++)
             {
                 sum += Math.Pow(point1[i] - point2[i], 2);
             }
-            return Math.Sqrt(sum);
+            return Math.Round(Math.Sqrt(sum),3);
         }
     }
 
     public class Patrol
     {
-        public Patrol(float x, float y, float z, string instructions, string url, long id64 = -1, string patrolType = null)
+
+        public Patrol(string patrolType, string category, double x, double y, double z, string instructions, string url)
         {
+            PatrolType = patrolType;
+            this.category = category;
             X = x;
             Y = y;
             Z = z;
             Instructions = instructions;
             Url = url;
-            Id64 = id64;
-            PatrolType = patrolType;
+        }
+
+        public Patrol(string category, double x, double y, double z, string instructions, string url)
+        {
+            this.category = category;
+            X = x;
+            Y = y;
+            Z = z;
+            Instructions = instructions;
+            Url = url;
         }
 
         public string PatrolType { get; set; }
+        public string category {  get; set; }
         public long Id64 { get; set; }
-        public float X { get; set; }
-        public float Y { get; set; }
-        public float Z { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
         public string Instructions { get; set; }
         public string Url { get; set; }
     }
