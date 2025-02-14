8b39daf44664ed524aed673ad06fddaade653570
Flynn <sindri.weinstein@gmail.com>
2025-02-14 11:06:36 +0100

    add new images.

--- src/EDDCanonn/Properties/Resources.Designer.cs
+++ src/EDDCanonn/Properties/Resources.Designer.cs
@@ -63,9 +63,49 @@ namespace EDDCanonn.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap biology {
+            get {
+                object obj = ResourceManager.GetObject("biology", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap canonn {
             get {
                 object obj = ResourceManager.GetObject("canonn", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap edd {
+            get {
+                object obj = ResourceManager.GetObject("edd", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap geology {
+            get {
+                object obj = ResourceManager.GetObject("geology", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ring {
+            get {
+                object obj = ResourceManager.GetObject("ring", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
