266fcfa0cd237e6974776f0b7faaacb2942c12a0
Flynn <sindri.weinstein@gmail.com>
2025-02-22 13:24:11 +0100

    add update image.

--- src/EDDCanonn/Properties/Resources.Designer.cs
+++ src/EDDCanonn/Properties/Resources.Designer.cs
@@ -159,5 +159,15 @@ namespace EDDCanonn.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap update {
+            get {
+                object obj = ResourceManager.GetObject("update", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
